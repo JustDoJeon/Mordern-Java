f5f70cf55d164cdff37afc450feb5c80249976ee
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2023-12-24 16:05:57 +0900

    innit commit

---
+++ src/test/java/com/dodo/mordernjava/MordernJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.mordernjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MordernJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
