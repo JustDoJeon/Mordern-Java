5d9b9f05246b57c9bac301a5a0e810452dd5fff8
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2023-12-24 16:05:57 +0900

    innit commit

---
+++ src/main/java/com/dodo/mordernjava/MordernJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.mordernjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MordernJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MordernJavaApplication.class, args);
+    }
+
+}
